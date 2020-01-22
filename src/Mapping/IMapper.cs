ab432f9d8a52e8804598cbe201a9279f8cff5181
Emmanuel Benitez <eben@aprico-consult.com>
2020-01-22 23:46:01 +0100

    Initial commit of project

---
+++ src/Mapping/IMapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+
+namespace BigSolution.Infra.Mapping
+{
+    [UsedImplicitly]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public interface IMapper<in TSource, out TDestination>
+    {
+        TDestination Map(TSource source);
+    }
+}
