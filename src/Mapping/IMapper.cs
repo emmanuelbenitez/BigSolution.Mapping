c5feca5913d431ecdcd40084ffcc6a6568ab1754
Emmanuel Benitez <emmanuel.benitez@gmail.com>
2021-01-24 22:46:45 +0100

    Update submodules, YAML pipeline & nuget

--- src/Mapping/IMapper.cs
+++ src/Mapping/IMapper.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License
 
-// Copyright © 2020 - 2020 Emmanuel Benitez
+// Copyright © 2020 - 2021 Emmanuel Benitez
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ namespace BigSolution.Infra.Mapping
     [UsedImplicitly]
     public interface IMapper<in TSource, out TDestination>
     {
+        [UsedImplicitly]
         TDestination Map(TSource source);
     }
 }
